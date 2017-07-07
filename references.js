b518aa816b479779572d2c9c484bea6d6fb94f47
David Jacobowitz <dgj@lbl.gov>
2017-07-06 17:18:59 -0700

    example references file

---
+++ references.js
@@ -0,0 +1,19 @@
+
+var references = {
+    'kirk': {
+        'author': 'James T. Kirk',
+        'year': '2015',
+        'link': 'https://en.wikipedia.org/wiki/James_T._Kirk',
+    },
+    'spock': {
+        'author': 'Spock',
+        'year': '2016',
+        'link': 'https://en.wikipedia.org/wiki/Spock',
+    },
+    'uhuru': {
+        'author': 'Lt. Cmdr Nyota Uhuru',
+        'year': '2017',
+        'link': 'https://en.wikipedia.org/wiki/Uhura',
+    },
+};
+
