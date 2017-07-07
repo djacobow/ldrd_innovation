70ddca7c77b35b435773ebc58d275be7884f2fd4
David Jacobowitz <dgj@lbl.gov>
2017-07-06 18:00:52 -0700

    file renaming

---
+++ init.js
@@ -0,0 +1,296 @@
+/* jshint esversion:6 */
+
+function makeScriptsRun(node) {
+    if (node.tagName === 'SCRIPT') {
+        node.parentNode.replaceChild( nodeScriptClone(node) , node );
+    }
+    else {
+        var i        = 0;
+        var children = node.childNodes;
+        while ( i < children.length ) {
+            makeScriptsRun( children[i++] );
+        }
+    }
+    return node;
+}
+
+function nodeScriptClone(node){
+    var script  = document.createElement("script");
+    script.text = node.innerHTML;
+    for( var i = node.attributes.length-1; i >= 0; i-- ) {
+        script.setAttribute( node.attributes[i].name, node.attributes[i].value );
+    }
+    return script;
+}
+
+
+
+
+var indata_by_id = {};
+
+function selectTab (evt, tabName) {
+    // console.log('selectTab ' + tabName);
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("contenttab");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    var contentName = tabName.replace('btn_','info_');
+    document.getElementById(contentName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+var makeTreeData = function(id) {
+    var od = [];
+    for (var i=0; i<id.length; i++) {
+        var leafdata = id[i];
+        var leaf = {};
+        leaf.icon = 'rt_chev_32.png';
+        leaf.id = leafdata.id;
+        leaf.text = leafdata.name;
+        leaf.parent = leafdata.parent ? leafdata.parent : '#';
+        od.push(leaf);
+    }
+    return od;
+};
+
+
+var fetchAndDo = function(target, resource, cb = null) {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener('load',function(e) {
+        // console.log(e);
+        if (cb) {
+            try {
+                var d = JSON.parse(xhr.response);
+                cb(target,d);
+            } catch(err) {
+                var pre = document.createElement('pre');
+                console.log(err);
+                pre.innerText = JSON.stringify(err,null,2);
+            }
+        } else {
+            target.innerHTML = xhr.response;
+        }
+        makeScriptsRun(target);
+    });
+    xhr.open('GET',resource);
+    xhr.send();
+};
+
+function makeid(len) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i=0; i<len; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+var elaborateFromJSON = function(target, data) {
+    console.log('elaborateJSON');
+    var iter = 0;
+    data.forEach(function(d) {
+        var div0 = document.createElement('div');
+        div0.className = 'section';
+        target.appendChild(div0);
+        var div00 = document.createElement('div');
+        div00.className = 'section_heading';
+        div00.innerText = d.head;
+        console.log('elaborating: ' + d.head);
+        div0.appendChild(div00);
+
+        var contents = d.contents;
+        if (contents.hasOwnProperty('paras')) {
+            contents.paras.forEach(function(para) {
+                console.log('doing para');
+                if (para.hasOwnProperty('subhead')) {
+                    var div01 = document.createElement('div');
+                    div01.className = 'section_subheading';
+                    div01.innerText = para.subhead;
+                    div0.appendChild(div01);
+                }
+                if (para.hasOwnProperty('text')) {
+                    var p = document.createElement('p');
+                    p.className = 'paragraph';
+                    p.innerText = para.text;
+                    div0.appendChild(p);
+                }
+            });
+        }
+
+        var div0n = document.createElement('div');
+        div0n.id = target.id + '_' + iter.toString();
+        div0.appendChild(div0n);
+
+        extra_args = {};
+        if (contents.hasOwnProperty('args')) extra_args = contents.args;
+
+        var copyanchor = null;
+        if (contents.hasOwnProperty('copyright')) {
+            if (contents.copyright.hasOwnProperty('href')) {
+                copyanchor = document.createElement('a');
+                copyanchor.className = 'section_copyright';
+                copyanchor.href = contents.copyright.href;
+                if (contents.copyright.hasOwnProperty('text')) {
+                    copyanchor.innerText = contents.copyright.text;
+                } else {
+                    copyanchor.innerText = contents.copyright.href;
+                }
+            }
+        }
+
+
+        var type = contents.hasOwnProperty('type') ? contents.type : '';
+
+        switch (contents.type) {
+            case 'raw_html':
+                div0n.innerHTML = contents.data;
+                break;
+            case 'paragraphs':
+                // any section can have paras; we deal with them above.
+                // They will always come before the element.
+                break;
+            case 'table':
+                if (contents.hasOwnProperty('file')) {
+                    makeTableFromCSV(div0n,contents.file);
+                } else if (contents.hasOwnProperty('data')) {
+                    div0n.appendChild(makeTableFromArry(contents.data));
+                }
+                break;
+            case 'pie':
+            case 'bar':
+            case 'line':
+                div0n.className = 'basic_chart';
+                console.log(contents.type);
+                makeChartFromCSV(contents.type,div0n,contents.file, extra_args);
+                break;
+            case 'gsheet':
+                var ifr = document.createElement('iframe');
+                ifr.className = 'basic_sheet';
+                ifr.src = contents.src;
+                div0n.appendChild(ifr);
+                break;
+            default:
+                // do nothing
+                break;
+        }
+
+        if (copyanchor) {
+            console.log('FGHIJKL');
+            div0.appendChild(copyanchor);
+            div0c = document.createElement('div');
+            div0c.className = 'section_copyright';
+            div0c.appendChild(copyanchor);
+            div0.appendChild(div0c);
+        }
+
+        console.log('appending');
+        iter += 1;
+    });
+    console.log(data);
+};
+
+var populateTabs = function(item_id, item_path) {
+    var i;
+    console.log('populateTabs: ' + item_id);
+
+    var tabLinksDiv = document.getElementById('tab_buttons');
+    while (tabLinksDiv.firstChild) {
+        tabLinksDiv.removeChild(tabLinksDiv.firstChild);
+    }
+    var infosDiv = document.getElementById('info_blobs');
+    while (infosDiv.firstChild) {
+        infosDiv.removeChild(infosDiv.firstChild);
+    }
+
+    document.getElementById('leafname').innerText = item_path.join(' \u21A0 ');
+    var leafdata = indata_by_id[item_id];
+
+    var first_button = null;
+    if (leafdata.hasOwnProperty('info')) {
+        var tab_ids = Object.keys(leafdata.info);
+        for (i=0; i<tab_ids.length; i++) {
+            var tabbutton = document.createElement('button');
+            if (i===0) first_button = tabbutton;
+            var tab_id = tab_ids[i];
+            tabbutton.id = 'btn_' + tab_id;
+            tabbutton.innerText = leafdata.info[tab_id].name;
+            tabbutton.className = 'tablinks';
+            /* jshint loopfunc: true */
+            tabbutton.addEventListener('click', function(ev) {
+                selectTab(ev,ev.target.id);
+            });
+            tabLinksDiv.appendChild(tabbutton);
+
+            var target = document.createElement('div');
+            var target_id = 'info_' + tab_id;
+            target.id = target_id;
+            var tab = leafdata.info[tab_id];
+            if (tab.hasOwnProperty('embed')) {
+                var nif = document.createElement('iframe');
+                nif.src = tab.embed;
+                target.appendChild(nif);
+            } else if (tab.hasOwnProperty('htfile')) {
+                fetchAndDo(target,tab.htfile,null);
+            } else if (tab.hasOwnProperty('jsfile')) {
+                fetchAndDo(target,tab.jsfile,elaborateFromJSON);
+            } else {
+                target.innerText = tab.data;
+            }
+            target.className = 'contenttab';
+            infosDiv.appendChild(target);
+        }
+
+        // kick off a fake event that will select the first
+        // tab
+        var event = new MouseEvent('click', {
+            currentTarget: first_button,
+            srcElement: first_button,
+        });
+        first_button.dispatchEvent(event);
+    }
+};
+
+var setup = function() {
+
+  for (var i=0; i<indata.length; i++) {
+      indata_by_id[indata[i].id] = indata[i];
+  }
+
+  $('#treediv').jstree({
+    'core': {
+      'themes': {
+        'variant': 'large',
+      },
+      // 'plugin': [ 'wholerow', ],
+      'data': makeTreeData(indata),
+    },
+  });
+
+ $('#treediv').on('changed.jstree', function(e, data) {
+     if (data.action == 'select_node') {
+         var path = data.node.parents.reverse();
+         path.push(data.node.text);
+         path.shift();
+         var id = data.node.id;
+         populateTabs(id,path);
+     }
+ });
+};
+
+google.charts.load('current', {'packages':['corechart','bar']});
+
+google.charts.setOnLoadCallback(function() {
+    console.log('google charts loaded');
+    setup();
+});
+
---
+++ macros.js
@@ -0,0 +1,284 @@
+/* jshint esversion:6 */
+var default_table_classes = {
+    table : 'csvTable',
+    top: 'csvTop',
+    uprlft: 'csvUpperLeft',
+    left: 'csvLeft',
+    erow: 'csvEven',
+    orow: 'csvOdd',
+    cell: 'csvCell',
+    ecol: '',
+    ocol: '',
+    right: 'csvRight',
+    bottom: 'csvBottom',
+    reflink: 'csvRefLink',
+};
+
+// cribbed from: http://stackoverflow.com/questions/8493195/how-can-i-parse-a-csv-string-with-javascript-which-contains-comma-in-data
+// Return array of string values, or NULL if CSV string not well formed.
+function csvLineToArray(text) {
+    var re_valid = /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/;
+    var re_value = /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g;
+    // Return NULL if input string is not well formed CSV string.
+    if (!re_valid.test(text)) return null;
+    var a = [];                     // Initialize array to receive values.
+    text.replace(re_value, // "Walk" the string using replace with callback.
+        function(m0, m1, m2, m3) {
+            // Remove backslash from \' in single quoted values.
+            if      (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"));
+            // Remove backslash from \" in double quoted values.
+            else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'));
+            else if (m3 !== undefined) a.push(m3);
+            return ''; // Return empty string.
+        });
+    // Handle special case of empty last value.
+    if (/,\s*$/.test(text)) a.push('');
+    return a;
+}
+
+
+var csvToArry = function(dstring) {
+    var oa = [];
+    var lines = dstring.split(/\r?\n/);
+    for (var j=0; j<lines.length; j++) {
+        var line = lines[j];
+        if (line.length) {
+            var row = csvLineToArray(line);
+            for (var k=0;k<row.length;k++) {
+                if (row[k].match(/^\d+\.?\d*$/)) {
+                    row[k] = parseFloat(row[k]);
+                }
+            }
+            oa.push(row);
+        }
+    }
+    return oa;
+};
+
+
+var makeTableFromCSV = function(target, url, classes = default_table_classes) {
+    console.log('makeTableFromCSV');
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET',url);
+    xhr.onload = function() {
+        // console.log(xhr);
+        if ((xhr.status === 200) || (xhr.status === 0)){
+            var ary = csvToArry(xhr.responseText);
+            var reflink = { text: url, href: url };
+            var table = makeTableFromArry(ary, classes,reflink);
+            target.appendChild(table);
+        } else {
+            target.innerText = 'Failed to load supporting data';
+        }
+    };
+    xhr.send();
+};
+
+var makeChartFromCSV = function(type, target, url, titles) {
+    console.log('makeBarChartFromCSV');
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET',url);
+    xhr.onload = function() {
+        // console.log(xhr);
+        if ((xhr.status === 200) || (xhr.status === 0)){
+            var ary = csvToArry(xhr.responseText);
+            var reflink = { text: url, href: url };
+            makeChartFromArry(type, target, ary, titles, reflink);
+        } else {
+            target.innerText = 'Failed to load supporting data';
+        }
+    };
+    xhr.send();
+};
+
+var makeChartFromArry = function(type, target, data, options = null, link = null) {
+    console.log('makeBarChartFromArry()');
+    var drawChart = function() {
+        console.log('drawChart()');
+        var cdata = google.visualization.arrayToDataTable(data);
+        // var chart = new google.charts.Bar(target);
+        // chart.draw(cdata, google.charts.Bar.convertOptions(options));
+        var chart = null;
+        switch (type) {
+            case 'bar': 
+                chart = new google.visualization.ColumnChart(target); 
+                break;
+            case 'pie': 
+                chart = new google.visualization.PieChart(target); 
+                break;
+            case 'line': 
+                chart = new google.visualization.LineChart(target); 
+                break;
+            default:
+                chart = new google.visualization.ColumnChart(target); 
+        }
+
+        chart.draw(cdata, options);
+    };
+
+    // This is a hack to unhide the div's hidden parent because
+    // Google Charts won't render properly in a hidden div
+    var contentdiv = target.parentNode.parentNode;
+    var cdiv_display = contentdiv.style.display; 
+    contentdiv.style.display = 'block';
+    drawChart();
+    // then return the content div to whatever it had been before
+    contentdiv.style.display = cdiv_display;
+
+    if (link) {
+        var ld = document.createElement('div');
+        ld.className = 'refLink';
+        var a = document.createElement('a');
+        a.innerText = link.text;
+        a.href = link.href;
+        ld.appendChild(a);
+        target.appendChild(ld);
+    }
+};
+
+
+var makeTableFromArry = function(data, classes = default_table_classes,
+                                 link = null, widths = null) {
+    var telem = document.createElement('table');
+    telem.className = classes.table;
+    var i,j;
+    var max_cols = 0;
+    for (j=0; j<data.length; j++) {
+        var tr = document.createElement('tr');
+        var tr_classes = [];
+        if (j === 0) tr_classes.push(classes.top);
+        else tr_classes.push(j % 2 ? classes.orow : classes.erow);
+        if (j === data.length-1) tr_classes.push(classes.bottom);
+        tr.className = tr_classes.join(' ');
+        var row = data[j];
+        if (row.length > max_cols) max_cols = row.length;
+        for (i=0;i<row.length;i++) {
+            var td = document.createElement('td');
+            var td_classes = [];
+            if ((i === 0) && (j === 0)) td_classes.push(classes.uprlft);
+            if (i === 0) td_classes.push(classes.left);
+            else td_classes.push(i % 2 ? classes.ocol : classes.ecol);
+            if (i === row.length-1) td_classes.push(classes.right);
+            td.className = td_classes.join(' ');
+            var cell = row[i];
+            td.innerText = cell.toString();
+            if (widths && (i<widths.length)) {
+                td.width = widths[i].toString() +'%';
+            }
+
+            tr.appendChild(td);
+        }
+        telem.appendChild(tr);
+    }
+    // optionally add a final row that has a single td that contains
+    // a link to the source data file for download
+    if (link) {
+        var ltr = document.createElement('tr');
+        var ltd = document.createElement('td');
+        ltd.colSpan = max_cols;
+        ltd.className = classes.reflink;
+        var a = document.createElement('a');
+        a.href = link.href;
+        a.innerText = link.text;
+        ltd.appendChild(a);
+        ltr.appendChild(ltd);
+        telem.appendChild(ltr);
+    }
+    return telem;
+};
+
+var finalizeReferences = function(refs) {
+    if ((refs === null) || (refs === undefined))  refs = references;
+
+    var mentions = document.getElementsByClassName('reference');
+    var uses = {};
+
+    for (var j=0; j<mentions.length; j++) {
+        var refkey = mentions[j].getAttribute('ref');
+        if (refkey && refkey.length) {
+            console.log('refkey: ' + refkey);
+            ref_dst_anchor = '#ref_dst_' + refkey;
+            ref_src_anchor = '#ref_src_' + j.toString();
+            if (refs.hasOwnProperty(refkey)) {
+                var ref = refs[refkey];
+                console.log(ref);
+                if (!uses.hasOwnProperty(refkey)) uses[refkey] = [];
+                uses[refkey].push(j);
+
+                var a = document.createElement('a');
+                var b = document.createElement('sup');
+                b.innerText = (j+1).toString();
+                a.href = ref_dst_anchor;
+                a.appendChild(b);
+                mentions[j].appendChild(a);
+                mentions[j].id = ref_src_anchor;
+            }
+        }
+    }
+
+    var refshome = document.getElementById('refs_container');
+    var x0 = document.createElement('hr');
+    refshome.appendChild(x0);
+    var x1 = document.createElement('div');
+    x1.innerText = 'References';
+    refshome.appendChild(x1);
+    var t0 = document.createElement('table');
+    var use_keys = Object.keys(uses);
+    for (j=0; j<use_keys.length; j++) {
+        var use_key = use_keys[j];
+        // var ref = refs[use_key];
+        var tr0 = document.createElement('tr');
+        tr0.id = 'ref_dst_' + use_key;
+        var td0 = document.createElement('td');
+        var td1 = document.createElement('td');
+        for (var i=0;i<uses[use_key].length;i++) {
+            idx = uses[use_key][i];
+            var a1 = document.createElement('a');
+            a1.innerText = (idx + 1).toString();
+            a1.href = '#ref_src_' + idx.toString();
+            td0.appendChild(a1);
+            if (i != (uses[use_key].length-1)) {
+                var s1 = document.createElement('span');
+                s1.innerText = ', ';
+                td0.appendChild(s1);
+            }
+        }
+        // td0.innerText = uses[use_key].map(function(x) { return x+1; }).join(', ');
+        tr0.appendChild(td0);
+        var s0 = document.createElement('span');
+
+        var r = refs[use_key];
+        if (r.hasOwnProperty('title')) {
+            var r0 = document.createElement('span');
+            r0.className = 'reflist_title';
+            r0.innerText = r.title;
+            s0.appendChild(r0);
+        }
+        if (r.hasOwnProperty('author')) {
+            var r1 = document.createElement('span');
+            r1.className = 'reflist_author';
+            r1.innerText = ' ' + r.author;
+            s0.appendChild(r1);
+        }
+        if (r.hasOwnProperty('year')) {
+            var r2 = document.createElement('span');
+            r2.className = 'reflist_year';
+            r2.innerText = ' (' + r.year+ ')';
+            s0.appendChild(r2);
+        }
+        if (r.hasOwnProperty('link')) {
+            var r3 = document.createElement('a');
+            r3.className = 'reflist_link';
+            r3.href = r.link;
+            r3.innerText = ' <' + r.link + '>';
+            s0.appendChild(r3);
+        }
+        td1.appendChild(s0);
+
+        tr0.appendChild(td1);
+        t0.appendChild(tr0);
+    }
+    refshome.appendChild(t0);
+    
+};
+
